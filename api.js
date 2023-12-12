acab3832f20ed50cc331debc01d50c86d095b0c3
Asbayazitog1 <as_bayazit@outlook.com>
2023-12-12 10:25:29 +0000

    api paths

---
+++ api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const newsApi =axios.create({
+    baseURL: "https://nc-backendapi.onrender.com/api",
+  });
+
+export const getTopics =()=>{
+return newsApi.get(`/topics`)
+}
+
+export const getArticles =()=>{
+return newsApi.get(`/articles`)
+}
+export const getArticlesByArticleID =(article_id) =>{
+return newsApi.get(`/articles/${article_id}`)
+}
+export const getCommentByArticleID =(article_id)=>{
+return newsApi.get(`/api/articles/${article_id}/comments`)
+}
+
+export const addNewCommentByArticleId=(article_id,newComment)=>{
+    return newsApi.post(`/api/articles/${article_id}/comments`).send(newComment)
+}
+
+export const updateArticleByArticleId =(article_id,updatedArticle)=>{
+    return newsApi.patch(`/api/articles/${article_id}`).send(updatedArticle)
+}
+
+export const deleteCommentByCommentId=(comment_id)=>{
+    return newsApi.delete(`/api/comments/${comment_id}`)
+}
