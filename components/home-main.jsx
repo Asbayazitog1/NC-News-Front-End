6173a545b1059509f0b4256faf0e497df19900b5
Asbayazitog1 <as_bayazit@outlook.com>
2023-12-12 12:00:20 +0000

    loding function

--- components/home-main.jsx
+++ components/home-main.jsx
@@ -2,24 +2,30 @@ import { useEffect, useState } from "react"
 import { getArticles } from "../api"
 
 const HomeMain =() =>{
+    const[ isloading,setIsloading] =useState(true)
     const [articles,setArticles]= useState([])
     useEffect(()=>{
         getArticles().then(({data}) =>{
             setArticles(data.articles)
+            setIsloading(false)
         })
     },[])
-return <section>
-    
-    <ul id="article-list">
-        {    
-        articles.map(article =>{
-            return <section key={article.article_id} id="artile-list-item">
-                <li id="" >{article.title}</li>
-                <img src={article.article_img_url} alt="" id="article-img" />
-            </section> 
-        })
+    if(isloading){
+        return <h2>loading...</h2>
     }
-    </ul>
-</section>
+    return <section>
+    
+        <ul id="article-list">
+            {    
+            articles.map(article =>{
+                return <section key={article.article_id} id="artile-list-item">
+                    <li id="" >{article.title}</li>
+                    <img src={article.article_img_url} alt="" id="article-img" />
+                </section> 
+            })
+        }
+        </ul>
+    </section>
+
 }
 export default HomeMain
