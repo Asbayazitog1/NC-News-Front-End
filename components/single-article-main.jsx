600a9b795921ccaf0c1da0981ca03d9b3409fda5
Asbayazitog1 <as_bayazit@outlook.com>
2023-12-13 16:15:06 +0000

    eror message added

--- components/single-article-main.jsx
+++ components/single-article-main.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 import CommentList from "./comment-list";
 
 const SingleArticleMain =()=>{
-    
+    const [err,setErr] =useState("")
     const[ isloading,setIsloading] =useState(true)
     const [article,setArticle]=useState({})
     const{article_id} = useParams()
@@ -21,7 +21,7 @@ const SingleArticleMain =()=>{
     return {...currArticle,votes: currArticle.votes +1}
     })
     updateArticleByArticleId(article_id,{inc_votes:1}).catch((err) =>{
-        console.log(err,"---- eror")
+        setErr(err.message)
         setArticle((currArticle)=>{
             return {...currArticle,votes: currArticle.votes -1}
             })
@@ -32,10 +32,11 @@ const SingleArticleMain =()=>{
         return {...currArticle,votes: currArticle.votes -1}
         })
         updateArticleByArticleId(article_id,{inc_votes:-1}).catch((err) =>{
-            console.log(err,"---- eror")
+            setErr(err.message)
             setArticle((currArticle)=>{
                 return {...currArticle,votes: currArticle.votes +1}
                 })
+               
         })
    }
    if(isloading){
@@ -50,6 +51,7 @@ return <section>
     <p id="votes-number">{article.votes}</p>
     <button id="votes-plus-button" onClick={handlePlusClick}>+</button>
     <button id="votes-minus-button" onClick={handleMinusButton}>-</button>
+    <p>{err}</p>
     </section>
     
     <p>By: {article.author}</p>
